d4e4c071c5a2a0cf47624acfd4a7368525e8a7b6
enriqueAscencio <=>
2024-01-19 17:32:01 -0600

    promesas 7

--- promesas/promesas.js
+++ promesas/promesas.js
@@ -1,19 +1,30 @@
-const ejecutarConTimeout = (ms)=>{
+const sumarLento = (numero)=>{
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            resolve(`Operación completada depués de ${ms} milisegundos`);
-        }, ms);
+            //resolve(numero +1 );
+            reject('error al pasar dato')
+
+        },800);
+
+    })
+}
+
+const sumarRapido = (numero)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(numero +1 );
+
+        },300);
+
     })
 }
 
 
-const tiempoLimite = 5000;
-
-ejecutarConTimeout(tiempoLimite)
-    .then(resultado =>{
-        console.log(resultado);
-    } )
-    .catch(error =>{
-        console.log(error);
+Promise.all(sumarRapido(6), [sumarLento(5)])
+.then(respuestas=>{
+    console.log(respuestas)
     })
+.catch(console.log);
+
+
 
