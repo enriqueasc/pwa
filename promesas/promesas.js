5e0ad6ae99e17319fa742f40f2dafbd7623d3d04
enriqueAscencio <=>
2024-01-22 22:36:36 -0600

    nueve

--- promesas/promesas.js
+++ promesas/promesas.js
@@ -1,30 +1,38 @@
 const esperarNVeces = (veces)=>{
     return new Promise((resolve, reject)=>{
 
+
         let espera = 0;
-        
+
         for(let i = 1; i <= veces; i++){
             
-            espera = i * 1000;
+            espera += i * 1000;
+        }
+
+        console.log("aqui ", espera)
+
+
+        for(let i = 1; i <= veces; i++){
+            
 
             console.log(i)
 
             setTimeout(()=>{
-
-                console.log("espera ", espera)
             
                 if(i  === veces){
                     resolve(`¡He esperado ${veces} veces!`)
                 }
     
             },espera);
-
-            console.log(typeof espera)
         
         }
 
     })
 }
+
+
+
+
 
 esperarNVeces(3)
     .then(resultado =>{
