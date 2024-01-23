556e355577d948819cd6723f11eb03bdb9fd9044
enriqueAscencio <=>
2024-01-22 23:21:19 -0600

    fetch 5

--- promesas/promesas.js
+++ promesas/promesas.js
@@ -1,8 +1,22 @@
-fetch('https://www.wikipedia.org/')
-.then( res => res.text() )
-.then(html => {
-    document.open()
-    document.write(html)
-    document.close()
-})
+
+
+fetch('../img/uno.png')
+    .then(res => res.blob())  // Obtener la imagen como un objeto Blob
+    .then(blob => {
+
+        console.log('Imagen Blob:', blob);
+        // Crear una URL para la imagen Blob
+        const imgUrl = URL.createObjectURL(blob);
+
+        // Crear la etiqueta de la imagen y establecer el atributo src
+        const imgElement = document.createElement('img');
+        imgElement.src = imgUrl;
+
+        // Insertar la imagen en el cuerpo del documento
+        document.body.appendChild(imgElement);
+    })
+    .catch(error => {
+        console.error('Error al cargar la imagen:', error);
+    });
+
 
