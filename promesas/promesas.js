b1f305d539d737b0544c39ce6a8ab9229702ac1b
Enrique Ascencio Martinez <e.asencio@impactosdigitales.com>
2024-01-24 17:50:11 -0600

    fecth 7 dos

--- promesas/promesas.js
+++ promesas/promesas.js
@@ -1,14 +1,20 @@
 
 
-fetch('https://fakestoreapi.com/products/1')
-    .then( res =>res.json() )
-    .then( data => {
-        if(data){
-            return console.log(data);
+fetch('https://fakestoreapi.com/products/s')
+    .then( res =>{
+
+        res.json();
+
+        if(res.ok){
+            return console.log(res);
         }else{
             throw new Error('no existe el producto')
         }
+
     })
+    .then( data => console.log(data))
+    
+   
     .catch( err => console.log(err))
 
         
